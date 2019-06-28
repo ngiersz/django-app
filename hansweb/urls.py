06f904961d25778fd99b0394ac6318116ee7a873
ngiersz <ngiersz@gmail.com>
2019-06-29 00:57:22 +0200

    Add simple order details and deleting an order

--- hansweb/urls.py
+++ hansweb/urls.py
@@ -5,4 +5,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='hansweb/home.html'), name='home'),
     path('orders/', views.orders, name='orders'),
+    path('orders/<int:pk>/', views.order_delete, name='order_delete'),
+    path('order/<int:pk>/', views.order_details, name='order_details'),
 ]
--- hansweb/views.py
+++ hansweb/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Order
 
+
 def home(request):
     return render(request, 'hansweb/home.html', {})
+
 
 def orders(request):
     user = request.user
@@ -12,3 +14,12 @@ def orders(request):
     else:
         return render(request, 'hansweb/orders.html', {'orders': None, 'user': user})
 
+
+def order_delete(request, pk):
+    Order.objects.filter(pk=pk).delete()
+    return orders(request)
+
+
+def order_details(request, pk):
+    order = Order.objects.get(pk=pk)
+    return render(request, 'hansweb/order_details.html', {'order': order})
