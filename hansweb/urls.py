faeda0e0c0bc613edeab4f2762e51da869757256
ngiersz <ngiersz@gmail.com>
2019-07-01 12:24:12 +0200

    Add signup

--- hansweb/urls.py
+++ hansweb/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='hansweb/home.html'), name='home'),
+    path('signup', views.signup, name='signup'),
     path('orders/', views.orders, name='orders'),
     path('orders/<int:pk>/', views.order_delete, name='order_delete'),
     path('order/<int:pk>/', views.order_details, name='order_details'),
--- hansweb/views.py
+++ hansweb/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Order
 from .forms import OrderForm
 
 
 def home(request):
     return render(request, 'hansweb/home.html', {})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'hansweb/signup.html', {'form': form})
 
 
 def orders(request):
