5808ea3b5371ee78e794e027b5a3b204d7c94684
ngiersz <ngiersz@gmail.com>
2019-07-08 15:22:59 +0200

    Fix: accept and delete button

--- hansweb/urls.py
+++ hansweb/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('account/', views.account_view, name='account_view'),
     path('orders_all/', views.orders_all_waiting_available_view, name='orders_all_view'),
     path('orders/', views.orders_view, name='orders_view'),
-    path('orders/<int:pk>/', views.order_delete_view, name='order_delete_view'),
+    path('orders/<int:pk>/accept/', views.order_accept_view, name='order_accept_view'),
+    path('orders/<int:pk>/delete/', views.order_delete_view, name='order_delete_view'),
     path('order/<int:pk>/', views.order_details_view, name='order_details_view'),
     path('orders/new/', views.OrderFormWizardView.as_view(views.OrderFormWizardView.FORMS), name='order_add_view'),
-    path('orders/<int:pk>/', views.order_accept_view, name='order_accept_view'),
     path('orders_accepted/', views.orders_accepted_view, name='orders_accepted_view'),
 ]
