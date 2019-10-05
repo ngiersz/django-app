3c4c088d43cf09021cc6a4e1d7b5796814a815ee
ngiersz <ngiersz@gmail.com>
2019-10-05 17:51:56 +0200

    Add custom user admin view

--- hansweb/admin.py
+++ hansweb/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Order, Address
-from users.models import User
 
 admin.site.register(Order)
 admin.site.register(Address)
-admin.site.register(User)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin')
+    search_fields = ()
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, UserAdmin)
