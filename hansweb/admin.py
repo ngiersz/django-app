8681621e5c3dbff4f7972e0684d968aa5ede58f5
ngiersz <ngiersz@gmail.com>
2019-10-06 16:56:13 +0200

    Add custom order admin console

--- hansweb/admin.py
+++ hansweb/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Order, Address
 
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'client', 'deliverer', 'created_date', 'isPaid', 'pickupAddress', 'deliveryAddress')
+    search_fields = ['title']
+    readonly_fields = ()
+
+    filter_horizontal = []
+    list_filter = ['status']
+    fieldsets = ()
+
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Address)
