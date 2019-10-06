f58057a715985b5b104b1ec94aa3d502b17777f6
ngiersz <ngiersz@gmail.com>
2019-10-06 16:45:08 +0200

    Fix migrations: custom user in 0001 migration in hansweb app

--- hansweb/migrations/0001_initial.py
+++ hansweb/migrations/0001_initial.py
@@ -13,6 +13,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('users', '0001_initial'),
+
     ]
 
     operations = [
